c12c4a9f694f82de24215052787962af67403c2a
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-11-12 13:19:21 +0900

    1

--- 10_spring-web-mvc/01_lecture-source/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/ClassMappingController.java
+++ 10_spring-web-mvc/01_lecture-source/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/ClassMappingController.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.requestmapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,6 +28,23 @@ public class ClassMappingController {
     // 2. 여러 URL 매핑하기
     // modify, delete 동시 처리
     @RequestMapping(value = {"modify", "delete"}, method = RequestMethod.POST)
+    public String modifyAndDelete(Model model) {
+        model.addAttribute("message", "POST 방식의 수정, 삭제 둘 다 처리하는 핸들러 메소드 호출됨...");
+        return "mappingResult";
+    }
 
+    // 3. Path variable (url 경로를 타고 오는 값)
+    /* 필기.
+    *   @PathVariable 어노테이션을 통해 요청 URL 로부터 변수를 받아올 수 있다.
+    *   path variable 로 전달되는 {변수명] 값은 반드시 매개변수 명과 일치해야 한다.
+    *   만약 동일하지 않으면 @PathVariable("이름")을 설정해야 한다.
+    * */
+    @GetMapping("/detail/{orderNo}")
+    public String detail(Model model, @PathVariable int orderNo) {
+
+        model.addAttribute("message", orderNo + "번 주문 상세 조회 핸들러 메소드 호출됨...");
+        return "mappingResult";
+
+    }
 
 }
