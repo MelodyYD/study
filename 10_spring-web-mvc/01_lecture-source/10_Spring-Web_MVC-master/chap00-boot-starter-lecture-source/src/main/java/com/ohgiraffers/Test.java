16b3da0839b4ad37f6a12e5be2aa1f8283a46786
DESKTOP-OSFKUPN <epyneoz27@gmail.com>
2024-11-20 16:29:17 +0900

    1

---
+++ 10_spring-web-mvc/01_lecture-source/10_Spring-Web_MVC-master/chap00-boot-starter-lecture-source/src/main/java/com/ohgiraffers/Test.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+}
---
+++ 10_spring-web-mvc/01_lecture-source/10_Spring-Web_MVC-master/chap00-boot-starter-lecture-source/src/main/java/com/ohgiraffers/boot/ContextConfig.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.boot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.ohgiraffers")
+@Configuration
+public class ContextConfig {
+}
